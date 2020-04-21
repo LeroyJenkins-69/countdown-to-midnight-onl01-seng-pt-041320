084fe144e5220856f54858834fa49053930d1e3f
Kordell Parrish <parrishkordell@gmail.com>
2020-04-21 20:57:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,21 +1,18 @@
-  
-def countdown(number = 5)
+def countdown(number)
+  number = 10
   while number > 0
   puts "#{number} SECOND(S)!"
-  "#{number} SECOND(S)!"
   number -= 1
- end
- "HAPPY NEW YEAR!"
+  end
+  "HAPPY NEW YEAR!"
 end
-countdown
 
-def countdown_with_sleep(number = 5)
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  "#{number} SECOND(S)!"
-  sleep 1
-  number -= 1
- end
- "HAPPY NEW YEAR!"
+def countdown_with_sleep(n)
+  n = 5
+  while n > 0 
+  puts "#{n} SECOND(S)!"
+  n -= 1 
+  sleep 1 
+  end
+  "HAPPY NEW YEAR!"
 end
-countdown_with_sleep
